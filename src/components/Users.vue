cac4c21fe361430467b3cabc888dc65425e6472f
KarenChuang <magic_gua@163.com>
2017-02-28 17:23:44 +0800

    modify:add filter function

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <ul>
-      <li v-for="user in unregisteredUsers">
+      <li v-for="user in users">
         {{user.name}} <button @click="join(user)">join us</button>
       </li>
     </ul>
@@ -15,13 +15,10 @@
     // },
     computed: {
       users() {
-        return this.$store.state.usersList
-      },
-      unregisteredUsers() {
-        return this.users.filter((user) => {
+        return this.$store.state.usersList.filter((user) => {
           return !user.registered
         })
-      }
+      },
     },
     methods: {
       join (user) {
